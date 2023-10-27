9487a26a7d0567447320244ae7248c8ad2def3cc
Juan José <jledesma2509@gmail.com>
2023-10-27 12:46:54 -0500

    Carga inicial

---
+++ app/src/main/java/com/jotadev/ecoeatsapp/presentation/common/Components.kt
@@ -0,0 +1,12 @@
+package com.jotadev.ecoeatsapp.presentation.common
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerComponent(modifier : Modifier = Modifier) {
+    Spacer(modifier = modifier)
+}
